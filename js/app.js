1e3008d1b314bf5e604a0f5596b378c8b11e9b6a
luigidis <luigi.distefano69@gmail.com>
2022-08-23 16:14:21 +0200

    Milestone + Bonus

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const app = new Vue ({
+    el: '#root',
+    data: {
+        title: 'Hello from data Vue!!',
+        image: 'https://picsum.photos/seed/picsum/400/400',
+        alt: 'lorem picsum'
+    }
+})
